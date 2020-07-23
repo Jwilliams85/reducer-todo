9f88150dff1b475fa91480df703ef4f069d55170
Janneth Williams <47696793+Jwilliams85@users.noreply.github.com>
2020-07-23 18:35:47 -0400

    changes

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,5 +1,4 @@
 import React,{useState,useReducer} from 'react';
-// import TodoList from './components/TodoList'
 import TodoForm from './components/TodoForm'
 import TodoList from './components/TodoList'
 
